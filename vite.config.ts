327beb643ed1bc870b0d1790c678e38c8e73504a
Dhairy <joshidhairy1994@gmail.com>
2026-01-05 12:14:20 +0530

    Simplify vite.config.ts to fix Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const srcPath = path.resolve(__dirname, "./src").replace(/\\/g, "/");
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -16,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": srcPath,
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
