3494ea354547240ecd83f31b4cedf884d39a759b
Dhairy <joshidhairy1994@gmail.com>
2026-01-05 12:22:57 +0530

    Remove conflicting vite.config.js and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
